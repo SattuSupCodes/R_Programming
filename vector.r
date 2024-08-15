082b8f77820c8ec2740d679d45362c01570c6c00
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-16 00:11:31 +0530

    upd

--- vector.r
+++ vector.r
@@ -1,4 +1,9 @@
 # a group of data elements of same type stored in a sequence in R
 vec_1 <- c(13, 65.9)
 vec_1
+
+# atomic vectors
+# logical, integer, double, character
+
+
 # pipe used to express a sequence of multiple operations represented with %>%
