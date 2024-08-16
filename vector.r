05b52e40433c47548c0d76e3a1ccd05d2d9d8ee4
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-16 14:35:54 +0530

    upd

--- vector.r
+++ vector.r
@@ -1,6 +1,12 @@
 # a group of data elements of same type stored in a sequence in R
 vec_1 <- c(13, 65.9)
 vec_1
+typeof(c(12.5))
+length(vec_1)
+is.character(vec_1) #false
+# creating lists
+list_1 <- list(12, "a", 7.9)
+list_1
 
 # atomic vectors
 # logical, integer, double, character
