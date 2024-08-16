1b2e9bf527f49551f634691ac882296619b1c66f
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-16 15:44:18 +0530

    upd

--- conditions.r
+++ conditions.r
@@ -5,3 +5,15 @@ if(x > 0) {
   print("x is a positive number")
 
 }
+
+x <- 7
+
+if (x > 0) {
+
+ print ("x is a positive number")
+
+} else {
+
+ print ("x is either a negative number or zero")
+
+}
