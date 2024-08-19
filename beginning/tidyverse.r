036b6d6392bea97c9f31decd428bf66b415287c5
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-20 02:39:02 +0530

    upd

--- beginning/tidyverse.r
+++ beginning/tidyverse.r
@@ -8,4 +8,4 @@ gfg <- data.frame(x = c("a", "b", "c", "d", "e"),  y = c(4, 5, 6, 7, 8)
 
 ggplot(gfg, aes(x, y, fill = x)) + geom_bar(state = "identity")
 
-# bhaiya ye kaisi language hai jisko itna zyada saja k elikhn apadhta hai bc
+# bhaiya ye kaisi language hai jisko itna zyada saja ke likhna padhta hai bc
