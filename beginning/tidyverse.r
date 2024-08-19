3728a70140796ec29cc37b0bccc2f2717a70ba14
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-20 02:58:09 +0530

    upd

--- beginning/tidyverse.r
+++ beginning/tidyverse.r
@@ -6,6 +6,7 @@ library("ggplot2")
 gfg <- data.frame(x = c("a", "b", "c", "d", "e"),  y = c(4, 5, 6, 7, 8)
 )
 
-ggplot(gfg, aes(x, y, fill = x)) + geom_bar(state = "identity")
+ggplot(gfg, aes(x, y, fill = x)) + geom_bar(stat = "identity")
 
 # bhaiya ye kaisi language hai jisko itna zyada saja ke likhna padhta hai bc
+
