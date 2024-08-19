55ddae392e7efebc1f8a04955236c8565f024f0a
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-20 01:39:08 +0530

    upd

--- beginning/plot.r
+++ beginning/plot.r
@@ -1,2 +1,11 @@
 library(datasets)
 head(iris)
+
+
+## Default S3 method:
+# plot(x, y = NULL, type = "p",  xlim = NULL, ylim = NULL,
+#      log = "", main = NULL, sub = NULL, xlab = NULL, ylab = NULL,
+#      ann = par("ann"), axes = TRUE, frame.plot = axes,
+#      panel.first = NULL, panel.last = NULL, asp = NA,
+#      xgap.axis = NA, ygap.axis = NA,
+#      ...)
