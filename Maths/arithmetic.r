9525fd34326a82c232684015d05ffde740403f54
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-23 21:26:11 +0530

    upd

--- Maths/arithmetic.r
+++ Maths/arithmetic.r
@@ -9,3 +9,10 @@ exp(x=6)
 
 value <- 3**2 + 4**1/8
 print(value)
+
+foo <- 5.3
+bar <- foo:(-47+1.5)
+print(bar) #we start with 5.3 then continue the sequence
+
+seq(from=4, to= 24, by=2) #another way of sequencing
+
