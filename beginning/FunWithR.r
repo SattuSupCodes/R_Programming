93ebdba1a1e7681e12ab37f01c4bafb9da98cfbb
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-25 18:07:56 +0530

    upd

--- beginning/FunWithR.r
+++ beginning/FunWithR.r
@@ -4,4 +4,4 @@ library('palmerpenguins')
 install.packages('ggplot2')
 library('ggplot2')
 
-ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(color="purple")
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(color="purple") # nolint
---
+++ beginning/listAndDataFrames.r
@@ -0,0 +1,3 @@
+# List of objects
+list <- list(matrix(data = 1:7, nrow = 2, ncol = 2), c(1,2,3,4), "hello")#can store multiple data types # nolint
+print(list)
