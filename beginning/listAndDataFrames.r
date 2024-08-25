5f32551c13f00eb166c1e95ea8bece39dc64630e
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-25 18:23:42 +0530

    upd

--- beginning/listAndDataFrames.r
+++ beginning/listAndDataFrames.r
@@ -1,3 +1,6 @@
 # List of objects
 list <- list(matrix(data = 1:7, nrow = 2, ncol = 2), c(1,2,3,4), "hello")#can store multiple data types # nolint
 print(list)
+
+list[[3]] #indexing
+list[[1]] + 4.5 #once indexed, can be treated as individual object
