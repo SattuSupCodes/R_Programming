8d107c66b5c26c9f2db91ceb7184cba66f00a36a
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-09-09 13:13:49 +0530

    upd

--- beginning/variables.r
+++ beginning/variables.r
@@ -6,3 +6,18 @@ typeof(str)
 
 num2 <- 5.88
 typeof(num2)
+
+vec <- c(2L, 3L, 4L, 5L, "hello")
+typeof(vec)
+class(vec)
+length(vec)
+
+com <- 3 + 4i
+typeof(com)
+
+lg <- log(x = 5, base = 10)
+print(lg)
+typeof(lg)
+
+length(str)
+class(str)
