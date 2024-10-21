da012ecba15bdcd3ae7054cfee5ed6622357732c
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-10-22 00:53:53 +0530

    r daddy fuck me harder

--- beginning/remove_na.r
+++ beginning/remove_na.r
@@ -4,3 +4,9 @@ v_clean <- c[! is na(v)] # nolint: error.
 print(v_clean)
 print(v)
 # smt fucked up
+
+        # Generate random numbers
+        random_numbers <- rnorm(10, mean = 0, sd = 1)
+
+        # Print random numbers
+        print(random_numbers)
