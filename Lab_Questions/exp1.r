8988ba9a21ef2d6cac6d2b9766f9535d95b47d09
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-11-13 14:18:31 +0530

    r programming hehe:

--- Lab_Questions/exp1.r
+++ Lab_Questions/exp1.r
@@ -1,3 +1,11 @@
-# wap to read data from various sources in a data frame.
+# create vectors, list, and dataframe 
 
-print("mujhse nhi horha bencho mai nhi krhi coding shoding")
+vec_1 <- c(1, 2, 3, 5)
+list_1 <- list(1,2,"sattu",1.5)
+df <- data.frame(c(1,2,3))
+
+# create 3x2matrix with inputs 10, 20, 30, 40
+a<- c(10,20,30, 40, 50, 60)
+mtr <- matrix(a, ncol=3, nrow=2)
+print(mtr)
+
