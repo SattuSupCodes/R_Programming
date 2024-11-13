7e723a56eb95f22e8eb5e2c405ec609f25d4ed6b
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-11-13 14:42:45 +0530

    r programming hehe:

--- Lab_Questions/exp1.r
+++ Lab_Questions/exp1.r
@@ -11,6 +11,34 @@ print(mtr)
 
 # create 2x2 subset matrix from this matrix
 
-
 subset_mtr <- mtr[, 1:2] 
 print(subset_mtr)
+
+# find the minimum and maximum value in matrix
+a<- c(10,20,30, 40, 50, 60)
+mtr <- matrix(a, ncol=3, nrow=2)
+print(mtr)
+
+min <- min(mtr)
+max <- max(mtr)
+print(min)
+print(max)
+
+# create function to print first n numbers
+
+print_first_n <- function(n) {
+ 
+  if (n <= 0 || n %% 1 != 0) {
+    cat("Please enter a positive integer.\n")
+    return()
+  }
+  
+ 
+  for (i in 1:n) {
+    cat(i, " ")
+  }
+  cat("\n")  
+}
+
+
+print_first_n(8)  
